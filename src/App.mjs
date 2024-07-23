d2e838d51100610fec62e7a4b88fd90459833a77
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-23 17:31:13 +0500

    Login Auth

--- src/App.mjs
+++ src/App.mjs
@@ -17,6 +17,27 @@ app.use(session({
     }
 }))
 
+app.post('/api/auth', async (request, response) => {
+    const { body: { username, password } } = request;
+
+    try {
+        const res = await pool.query('Select * from Users where username = $1', [username]);
+
+        if (res.rowCount === 0)
+            return response.status(401).send('Invalid Username');
+        if (res.rows[0].password !== password)
+            return response.status(401).send('Incorrect Password');
+
+        const User = res.rows[0];
+
+        request.session.user = User;
+        response.cookie('Cricket', 'Wicket', { maxAge: 60000 * 10 });
+        return response.status(200).send(User);
+    } catch (err) {
+        console.log(err.message);
+    }
+})
+
 app.listen(PORT, () => {
     console.log(`Server listening on PORT : ${PORT}`);
 })
