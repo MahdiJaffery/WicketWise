21d5d9677435a608d2c8746d44bb7da7a76e6603
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-23 17:23:17 +0500

    Cookies and Session

--- src/App.mjs
+++ src/App.mjs
@@ -1,6 +1,21 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import session from 'express-session';
+
+import pool from '../Utils/Database.mjs';
+
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(cookieParser());
+app.use(session({
+    secret: 'Wicket',
+    saveUninitialized: false,
+    cookie: {
+        maxAge: 60000 * 10
+    }
+}))
 
 app.listen(PORT, () => {
     console.log(`Server listening on PORT : ${PORT}`);
