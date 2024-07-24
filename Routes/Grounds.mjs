a166b9d78abf10888ba398d2b84f5c0a2d7b7d53
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-24 14:06:53 +0500

    Add Grounds

--- Routes/Grounds.mjs
+++ Routes/Grounds.mjs
@@ -16,4 +16,29 @@ router.get('/', validationCheck, async (request, response) => {
     }
 })
 
+router.post('/addGround', validationCheck, async (request, response) => {
+    const { body: { name, location, price } } = request;
+
+    if (!(name && location && price))
+        return response.status(400).send('Enter\nName: <Name>,\nLocation: <Location>,\nPrice: <Price>');
+
+    try {
+        let res = await pool.query('Select * from Grounds where name = $1 and location = $2', [name, location]);
+
+        if (res.rowCount !== 0)
+            return response.status(400).send('Ground Already Exists in Database :)');
+
+        res = await pool.query('Select * from Grounds');
+
+        const GroundID = res.rowCount + 1;
+
+        res = await pool.query('Insert into Grounds values ($1, $2, $3, $4)', [GroundID, name, location, price]);
+
+        return response.status(201).send('Ground Added to Database :)');
+    } catch (err) {
+        console.log(err.message);
+        return response.sendStatus(500);
+    }
+})
+
 export default router;
