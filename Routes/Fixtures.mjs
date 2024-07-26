4081e26f5ea11189fe9fec732303d7a01c6a1cc8
Mahdi Jaffery <mahdijaffri5@gmail.com>
2024-07-26 17:20:44 +0500

    Fixtures

---
+++ Routes/Fixtures.mjs
@@ -0,0 +1,40 @@
+import { Router } from "express";
+
+import pool from "../Utils/Database.mjs";
+import { checkAuthorisation, validationCheck } from "../Utils/Middleware.mjs";
+
+const router = Router();
+
+router.get('/', validationCheck, checkAuthorisation, async (request, response) => {
+    const { body: { tournamentid } } = request;
+
+    if (!tournamentid)
+        return response.status(400).send('Enter\ntournamentid: tournamentid');
+
+    let res;
+    let Teams = [], Fixtures = [];
+    try {
+        res = await pool.query('Select * from Teams where status = $1 and tournamentid = $2', ['Registered', parseInt(tournamentid)]);
+
+        if (!res.rowCount)
+            return response.status(404).send('No Teams Registered for This Tournament');
+
+        for (let i = 0; i < res.rowCount; i++)
+            Teams.push(res.rows[i].teamname);
+
+        let matchCount = 0;
+        for (let i = 0; i < Teams.length; i++)
+            for (let j = i + 1; j < Teams.length; j++) {
+                // console.log(Teams[i] + 'vs' + Teams[j]);
+                matchCount++;
+                Fixtures.push(`Match ${matchCount}:     ${Teams[i]}   vs   ${Teams[j]}`);
+            }
+
+        return response.status(200).send(Fixtures);
+    } catch (err) {
+        console.log(err.message);
+        return response.sendStatus(500);
+    }
+})
+
+export default router;
--- src/Index.mjs
+++ src/Index.mjs
@@ -3,7 +3,7 @@ import cookieParser from 'cookie-parser';
 import session from 'express-session';
 import { Router } from 'express';
 
-import pool from '../Utils/Database.mjs';
+import routeFixtures from '../Routes/Fixtures.mjs'
 import routeBookings from '../Routes/Booking.mjs'
 import routeGrounds from '../Routes/Grounds.mjs'
 import routeTournaments from '../Routes/Tournaments.mjs'
@@ -31,5 +31,6 @@ router.use('/api/contactus', routeContact);
 router.use('/api/playerstats', routeStats);
 router.use('/api/matches', routeMatches);
 router.use('/api/leaderboard', routeLeaderboard);
+router.use('/api/fixtures', routeFixtures);
 
 export default router;
